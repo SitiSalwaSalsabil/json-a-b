d521fe0a6cb16e2f48cffcd8b508254fcd1ae15d
sitisalwasalsabil3110 <sitisalwasalsabil3110@gmail.com>
2024-08-09 06:26:13 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  Future<List<User>> fetchUsers() async {
+    final response = await http.get(Uri.parse(
+        'https://ws.jakarta.go.id/gateway/DataPortalSatuDataJakarta/1.0/satudata?kategori=dataset&tipe=detail&url=indeks-kepuasan-layanan-penunjang-urusan-pemerintahan-daerah-pada-dinas-pariwisata-dan-ekonomi-kreatif'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['data'];
+      return data.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User List'),
+      ),
+      body: FutureBuilder<List<User>>(
+        future: fetchUsers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No users found'));
+          } else {
+            final users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return ListTile(
+                  title: Text(user.firstName),
+                  subtitle: Text(user.email),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class User {
+  final String firstName;
+  final String email;
+
+  User({required this.firstName, required this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      firstName: json['periode_data'],
+      email: json['jenis_layanan'],
+    );
+  }
+}
